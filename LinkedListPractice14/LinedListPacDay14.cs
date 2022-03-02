34c9ada507d771c24bcabf311387001bccd86ffc
GOPAL THAKUR <gopalk557@gmail.com>
2022-03-03 00:10:14 +0530

    [Gopal]Add:create LinkedList

---
+++ LinkedListPractice14/LinedListPacDay14.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListPractice14
+{
+    public class LinedListPacDay14
+    {
+        public Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linked List", node.data);
+        }
+
+    }
+}
--- LinkedListPractice14/Program.cs
+++ LinkedListPractice14/Program.cs
@@ -6,11 +6,16 @@ using System.Threading.Tasks;
 
 namespace LinkedListPractice14
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to LinkedList");
+            Console.WriteLine("Welcome to Linked List Operations \n");
+            LinedListPacDay14 list = new LinedListPacDay14();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
             Console.ReadKey(); ;
         }
     }
